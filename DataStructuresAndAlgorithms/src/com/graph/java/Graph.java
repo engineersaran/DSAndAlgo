a315b9ef08b167b1b817c6fa5d572681ab2b6a4e
Saravanakumar <engineersaran@gmail.com>
2018-02-23 12:17:28 +0530

    Depth First Search Addition for Graph

--- DataStructuresAndAlgorithms/src/com/graph/java/Graph.java
+++ DataStructuresAndAlgorithms/src/com/graph/java/Graph.java
@@ -1,17 +1,21 @@
 package com.graph.java;
 
+import java.util.Arrays;
+
 public class Graph {
 
 	private final int maximumVertices;
 	private Vertex[] vertexList;
 	private int[][] adjacencyMatrix;
 	private int vertexCount = 0;
+	private Stack stack;
 
 	public Graph(int maxVertices) {
 
 		maximumVertices = maxVertices;
 		vertexList = new Vertex[maximumVertices];
 		adjacencyMatrix = new int[maximumVertices][maximumVertices];
+		stack = new Stack(maximumVertices);
 		initializeAdjacencyMatrix();
 	}
 
@@ -55,11 +59,57 @@ public class Graph {
 	public void displayAdjacencyMatrix() {
 
 		for (int i = 0; i < maximumVertices; i++) {
-			System.out.print("AdjacencyList of Vertex : " + vertexList[i].vertexLabel +" --> ");
+			System.out.print("AdjacencyList of Vertex : " + vertexList[i].vertexLabel + " --> ");
 			for (int j = 0; j < maximumVertices; j++) {
-				System.out.print(adjacencyMatrix[i][j]+" ");
+				System.out.print(adjacencyMatrix[i][j] + " ");
 			}
 			System.out.println("");
+		}
+	}
+
+	public int getAdjUnvisitedVertex(String vertex) {
+
+		int vertexIndex = searchVertex(vertex);
+		for (int i = 0; i < maximumVertices; i++) {
+			if (adjacencyMatrix[vertexIndex][i] == 1 && vertexList[i].wasVisited == false) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	private int searchVertex(String vertex) {
+
+		Vertex searchedVertex = Arrays.asList(vertexList).stream().filter(t -> t.vertexLabel.equals(vertex)).findFirst()
+				.get();
+		return Arrays.asList(vertexList).indexOf(searchedVertex);
+	}
+
+	public void depthFirstSearch() {
+
+		vertexList[0].wasVisited = true;
+		displayVertex(0);
+		stack.push(vertexList[0].vertexLabel);
+
+		while (!stack.isEmpty()) {
+			int unVisitiedVertex = getAdjUnvisitedVertex(stack.peek());
+			if (unVisitiedVertex == -1) {
+				stack.pop();
+			} else {
+
+				vertexList[unVisitiedVertex].wasVisited = true;
+				displayVertex(unVisitiedVertex);
+				stack.push(vertexList[unVisitiedVertex].vertexLabel);
+			}
+
+		}
+
+		resetVertices();
+	}
+
+	public void resetVertices() {
+		for (int i = 0; i < maximumVertices; i++) {
+			vertexList[i].wasVisited = false;
 		}
 	}
 }
--- DataStructuresAndAlgorithms/src/com/graph/java/TestGraph.java
+++ DataStructuresAndAlgorithms/src/com/graph/java/TestGraph.java
@@ -6,20 +6,24 @@ public class TestGraph {
 		
 		Graph graph = new Graph(5);
 		graph.addVertex("A");
-		graph.addVertex("B");
+		graph.addVertex("F");
 		graph.addVertex("C");
-		graph.addVertex("D");
+		graph.addVertex("G");
 		graph.addVertex("E");
 		
 		graph.addEdge(0, 1);
 		graph.addEdge(0, 4);
-		graph.addEdge(1, 2);
+		graph.addEdge(0, 2);
 		graph.addEdge(1, 4);
 		graph.addEdge(2, 3);
 		graph.addEdge(3, 4);
 		
 		graph.displayVertices();
 		graph.displayAdjacencyMatrix();
+		
+		System.out.println("Visits: ");
+		graph.depthFirstSearch();
+		System.out.println();
 
 	}
 
